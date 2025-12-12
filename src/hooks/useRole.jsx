4d2a0c73ada8477a446e184d2dbd95fa7439eb34
nafiz512 <nafizmuntasir512@gmail.com>
2025-12-12 15:51:38 +0600

    added hooks

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import useAxios from "./useAxios";
 
 const useRole = () => {
-    const role = "admin";
-    const roleLoading = false;
+    const { user, loading } = useContext(AuthContext);
+    const axios = useAxios();
+
+    const [role, setRole] = useState("donor");
+    const [roleLoading, setRoleLoading] = useState(true);
+
+    useEffect(() => {
+        // Wait until Firebase finishes loading
+        if (loading) return;
+
+        // If no user logged in
+        if (!user?.email) {
+            setRole("donor");
+            setRoleLoading(false);
+            return;
+        }
+
+        // Fetch role
+        const fetchRole = async () => {
+            try {
+                const res = await axios.get(`/users/${user.email}/role`);
+                // console.log(res);
+
+                setRole(res.data?.role || "donor");
+            } catch (err) {
+                console.error(err);
+                setRole("donor");
+            } finally {
+                setRoleLoading(false);
+            }
+        };
+
+        fetchRole();
+    }, [loading, user?.email, axios]);
+
     return { role, roleLoading };
 };
 
---
+++ src/hooks/useSecureAxios.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useSecureAxios = () => {
+    return <div></div>;
+};
+
+export default useSecureAxios;
