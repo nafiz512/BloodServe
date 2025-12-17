e337fb6f30f1745b0046f31ee3543269dc5082a2
nafiz512 <nafizmuntasir512@gmail.com>
2025-12-17 16:43:14 +0600

    fix volunteer dashboard links

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -128,12 +128,24 @@ const DashboardLayout = () => {
                                 </span>
                             </Link>
                         </li>
+                        <li>
+                            <NavLink
+                                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                                data-tip="My Profile"
+                                to="/dashboard/profile"
+                            >
+                                <IoPerson></IoPerson>
+                                <span className="is-drawer-close:hidden">
+                                    My Profile
+                                </span>
+                            </NavLink>
+                        </li>
 
                         {/* dashboard links */}
                         <li>
                             <NavLink
                                 className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                data-tip="MyParcels"
+                                data-tip="Funding"
                                 to="/dashboard/funding"
                             >
                                 <RiRefundFill></RiRefundFill>
@@ -145,60 +157,14 @@ const DashboardLayout = () => {
 
                         {role === "volunteer" && (
                             <>
-                                <li>
-                                    <NavLink
-                                        className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip="Assigned Deliveries"
-                                        to="/dashboard/assigned-deliveries"
-                                    >
-                                        <FaTasks />
-                                        <span className="is-drawer-close:hidden">
-                                            Assigned Deliveries
-                                        </span>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip="Completed Deliveries"
-                                        to="/dashboard/completed-deliveries"
-                                    >
-                                        <SiGoogletasks />
-                                        <span className="is-drawer-close:hidden">
-                                            Completed Deliveries
-                                        </span>
-                                    </NavLink>
-                                </li>
+
+
                             </>
                         )}
 
                         {/* doner only links */}
                         {role === "donor" && (
                             <>
-                                <li>
-                                    <NavLink
-                                        className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip="My Profile"
-                                        to="/dashboard/profile"
-                                    >
-                                        <IoPerson></IoPerson>
-                                        <span className="is-drawer-close:hidden">
-                                            My Profile
-                                        </span>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip="My donation Requests"
-                                        to="/dashboard"
-                                    >
-                                        <LuNotebookText></LuNotebookText>
-                                        <span className="is-drawer-close:hidden">
-                                            My donation Requests
-                                        </span>
-                                    </NavLink>
-                                </li>
                                 <li>
                                     <NavLink
                                         className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
@@ -219,56 +185,30 @@ const DashboardLayout = () => {
                                 <li>
                                     <NavLink
                                         className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip="Donors Management"
-                                        to="/dashboard/donor-management"
+                                        data-tip="All Users"
+                                        to="/dashboard/all-users"
                                     >
                                         <FaUsers></FaUsers>
                                         <span className="is-drawer-close:hidden">
-                                            Donors Management
-                                        </span>
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                        data-tip=" Donation Requests"
-                                        to="/dashboard/donation-requests"
-                                    >
-                                        <BiSolidDonateBlood></BiSolidDonateBlood>
-                                        <span className="is-drawer-close:hidden">
-                                            Donation Requests
+                                            All Users
                                         </span>
                                     </NavLink>
                                 </li>
                             </>
                         )}
-
-                        {/* Settings */}
                         <li>
-                            <button
+                            <NavLink
                                 className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                                data-tip="Settings"
+                                data-tip="Donation Requests"
+                                to="/dashboard/donation-requests"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    className="my-1.5 inline-block size-4"
-                                >
-                                    <path d="M20 7h-9"></path>
-                                    <path d="M14 17H5"></path>
-                                    <circle cx="17" cy="17" r="3"></circle>
-                                    <circle cx="7" cy="7" r="3"></circle>
-                                </svg>
+                                <LuNotebookText></LuNotebookText>
                                 <span className="is-drawer-close:hidden">
-                                    Settings
+                                    Donation Requests
                                 </span>
-                            </button>
+                            </NavLink>
                         </li>
+
                     </ul>
                     <ul className="menu ">
                         <li onClick={handleLogout}>
